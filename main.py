824717e757f48fda388aab9159f81006df337724
RudgeJoao <joaorudge465@gmail.com>
2024-05-21 15:21:20 -0300

    [Feat] Melhorias na experiencia do jogador

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ import queue
 import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
-# driver = webdriver.Edge();
+driver = webdriver.Edge();
 
-# url = "https://archive.org/details/arcade_sfex2#"
-# driver.get(url)
-# driver.maximize_window()
+url = "https://archive.org/details/arcade_sfex2#"
+driver.get(url)
+driver.maximize_window()
 
+botao_ligar = driver.find_element(By.CLASS_NAME,"ghost")
+botao_ligar.click()
 
-# botaoLigar = driver.find_element(By.CLASS_NAME,"ghost")
-# botaoLigar.click()
+def countdown(seconds):
+    for i in range(seconds, -1, -1):
+        print(i)
+        time.sleep(1)
+
+countdown(60)
 
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
@@ -33,13 +42,13 @@ def draw_landmarks_on_image(rgb_image, landmarks):
 
 def detect_left_punch(landmarks):
     wrist_y = landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y
-    elbow_y = landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y
-    return wrist_y < elbow_y
+    shoulder_y = landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y
+    return wrist_y < shoulder_y
 
 def detect_right_punch(landmarks):
     wrist_y = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y
-    elbow_y = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y
-    return wrist_y < elbow_y
+    shoulder_y = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y
+    return wrist_y < shoulder_y
 
 def detect_left_kick(landmarks):
     knee_y = landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE].y
@@ -51,34 +60,78 @@ def detect_right_kick(landmarks):
     hip_y = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP].y
     return knee_y < hip_y
 
-def detect_jump(landmarks):
-    left_ankle_y = landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y
-    right_ankle_y = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE].y
-    hip_y = (landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP].y + landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP].y) / 2
-    return left_ankle_y < hip_y and right_ankle_y < hip_y
+def detect_flip(landmarks):
+    left_wrist_x = landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x
+    right_wrist_x = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x
+    left_elbow_x = landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x
+    right_elbow_x = landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x
+    
+    return left_wrist_x < right_elbow_x and right_wrist_x > left_elbow_x
+
+def detect_special(landmarks):
+
+    return 
+
+screen_flip_p1 = False
+screen_flip_p2 = False
 
 def process_frame(frame, pose, results_queue, player_label):
-    
     results = pose.process(frame)
+    detections = []
+    movement_detected = False
+
+    global screen_flip_p1
+    global screen_flip_p2
+
     if results.pose_landmarks:
         annotated_image = draw_landmarks_on_image(frame, results.pose_landmarks)
-        detections = []
         if detect_left_punch(results.pose_landmarks):
-            detections.append("Left punch detected")
+            detections.append("Soco esquerdo detectado")
             pyautogui.press('space' if player_label == "Jogador 1" else 'q')
+            movement_detected = True
+            
         if detect_right_punch(results.pose_landmarks):
-            detections.append("Right punch detected")
+            detections.append("Soco direito detectado")
             pyautogui.press('ctrl' if player_label == "Jogador 1" else 'a')
+            movement_detected = True
+            
         if detect_left_kick(results.pose_landmarks):
-            detections.append("Left kick detected")
+            detections.append("Chute esquerdo detectado")
             pyautogui.press('z' if player_label == "Jogador 1" else 'w')
+            movement_detected = True
+            
         if detect_right_kick(results.pose_landmarks):
-            detections.append("Right Kick detected")
+            detections.append("Chute direito detectado")
             pyautogui.press('x' if player_label == "Jogador 1" else 'e')
-        if detect_jump(results.pose_landmarks):
-            detections.append("Jump detected")
-            pyautogui.press('up' if player_label == "Jogador 1" else 'r')
-        results_queue.put((annotated_image, detections, player_label))
+            movement_detected = True
+            
+        if detect_flip(results.pose_landmarks):
+            detections.append("Flip detectada")
+            if player_label == 'Jogador 1' and not screen_flip_p1:
+                screen_flip_p1 = True
+            elif player_label == 'Jogador 1' and screen_flip_p1:
+                screen_flip_p1 = False
+            if player_label == 'Jogador 2' and not screen_flip_p2:
+                screen_flip_p2 = True
+            elif player_label == 'Jogador 2' and screen_flip_p2:
+                screen_flip_p2 = False
+            movement_detected = True
+            
+        if detect_special(results.pose_landmarks):
+            detections.append("Habilidade especial detectada")
+            movement_detected = True
+            
+        if not movement_detected:
+            detections.append("Nenhuma ação realizada")
+            if player_label == "Jogador 1" and not screen_flip_p1:
+                pyautogui.press('left')
+            elif player_label == "Jogador 1" and screen_flip_p1:
+                pyautogui.press('right')
+            if player_label == "Jogador 2" and not screen_flip_p2:
+                pyautogui.press('d')
+            elif player_label == "Jogador 2" and screen_flip_p2:
+                pyautogui.press('g')
+    results_queue.put((annotated_image, detections, player_label))
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
